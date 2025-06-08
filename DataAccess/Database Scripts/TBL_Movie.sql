ba14f2d7dfe8c62183b28230a73ad4e3754fa51f
haroldbaes17 <hbarrantese@ucenfotec.ac.cr>
2025-06-08 12:06:41 -0600

    Inclusion de tablas de usuarios y peliculas.

---
+++ DataAccess/Database Scripts/TBL_Movie.sql
@@ -0,0 +1,27 @@
+﻿USE [cenfocinemas-db]
+GO
+
+/****** Object:  Table [dbo].[TBL_Movie]    Script Date: 8/6/2025 12:03:44 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[TBL_Movie](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Created] [datetime] NOT NULL,
+	[Updated] [datetime] NULL,
+	[Title] [nvarchar](75) NOT NULL,
+	[Description] [nvarchar](250) NOT NULL,
+	[ReleaseDate] [datetime] NOT NULL,
+	[Genre] [nvarchar](20) NOT NULL,
+	[Director] [nvarchar](50) NOT NULL,
+ CONSTRAINT [PK_TBL_Movie] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+
