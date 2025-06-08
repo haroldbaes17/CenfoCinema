322bebe12d696d1a2ee246a931bf0b0011c8cb6a
haroldbaes17 <hbarrantese@ucenfotec.ac.cr>
2025-06-08 15:11:29 -0600

    SP UpdateUser.

---
+++ DataAccess/Database Scripts/SP_UpdateUser.sql
@@ -0,0 +1,18 @@
+﻿--SP para actualizar un User.
+CREATE PROCEDURE UPD_USER_PR
+	@P_Id int,
+	@P_UserCode nvarchar(30),
+	@P_Name nvarchar(50),
+	@P_Email nvarchar(50),
+	@P_Password nvarchar(50),
+	@P_BirthDate datetime,
+	@P_Status nvarchar(10)
+
+AS
+BEGIN
+   UPDATE TBL_User 
+   SET Updated = GETDATE(), UserCode = @P_UserCode, Name = @P_Name, Email = @P_Email, Password = @P_Password,
+   BirthDate = @P_BirthDate, Status = @P_Status
+   WHERE Id = @P_Id;
+END
+GO
--- DevTester/Program.cs
+++ DevTester/Program.cs
@@ -4,6 +4,9 @@ public class Program
 {
     public static void Main(string[] args)
     {
+        /*
+        // SP de Crear Usuario
+
         var sqlOperation = new SqlOperation();
         sqlOperation.ProcedureName = "CRE_USER_PR";
 
@@ -17,5 +20,27 @@ public class Program
         var sqlDao = SqlDao.GetInstance();
         
         sqlDao.ExecuteProcedure(sqlOperation);
+        */
+
+        // SP de Actualizar Usuario
+        var sqlOperation = new SqlOperation();
+        sqlOperation.ProcedureName = "UPD_USER_PR";
+
+        sqlOperation.AddIntParam("P_Id", 1);
+        sqlOperation.AddStringParameter("P_UserCode", "hbarrantes");
+        sqlOperation.AddStringParameter("P_Name", "Harold Barrantes");
+        sqlOperation.AddStringParameter("P_Email", "hbarrantese@ucenfotec.ac.cr");
+        sqlOperation.AddStringParameter("P_Password", "Cenfotec123!");
+        sqlOperation.AddDateTimeParam("P_BirthDate", new DateTime(2005, 12, 17));
+        sqlOperation.AddStringParameter("P_Status", "Activo");
+
+        var sqlDao = SqlDao.GetInstance();
+
+        sqlDao.ExecuteProcedure(sqlOperation);
+
+
+
+
+
     }
 }
