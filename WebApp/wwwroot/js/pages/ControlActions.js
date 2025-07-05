377f9ea86f4c606547041a8806a02809d5f4aeef
haroldbaes17 <hbarrantese@ucenfotec.ac.cr>
2025-07-05 11:14:08 -0600

    Cambio de ruta en el API

--- WebApp/wwwroot/js/pages/ControlActions.js
+++ WebApp/wwwroot/js/pages/ControlActions.js
@@ -1,7 +1,7 @@
 ﻿function ControlActions() {
 	//Ruta base del API
-	//this.URL_API = window.API_URL; 
-	this.URL_API = "https://localhost:7285/api/"; 
+	this.URL_API = window.API_URL; 
+	//this.URL_API = "https://localhost:7285/api/"; 
 
 	this.GetUrlApiService = function (service) {
 		return this.URL_API + service;
